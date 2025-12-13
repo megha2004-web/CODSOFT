ec8c7272bcb08e770eaf72a70df3daffa143d564
megha2004-web <meghad1411@gmail.com>
2025-12-13 13:14:47 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,28 @@
+let display = document.getElementById("display");
+
+function press(value) {
+    if(display.innerText=="0"){
+        display.innerText = value;
+    } else {
+        display.innerText += value;
+    }
+}
+
+function clearDisplay() {
+    display.innerText = "0";
+}
+
+function deleteLast() {
+    if(display.innerText.length == 1) {
+        display.innerText = 0;
+    }else{
+        display.innerText = display.innerText.slice(0,-1);
+    }
+}
+function calculate() {
+    try {
+        display.innerText = eval(display.innerText);
+    } catch {
+        display.innerText = "Error";
+    }
+}
